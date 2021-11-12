d474689e366bfe28e699ab63fcfacf851a58e8e2
acer <acer@LAPTOP-L7MUKSIE>
2021-11-12 11:40:29 +0700

    Remove Failure TC from Test suite

--- Test Suites/Demo/TestSuite1.ts
+++ Test Suites/Demo/TestSuite1.ts
@@ -14,7 +14,7 @@
    <testCaseLink>
       <guid>fbdd3741-e676-4c81-a9f4-ce714ac3d4c7</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Demo/ManualMode</testCaseId>
    </testCaseLink>
    <testCaseLink>
